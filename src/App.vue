809eac23be52b403c680bfb20c1ada3b4539d52f
joackimperochaud <45937163+joackimperochaud@users.noreply.github.com>
2021-02-01 12:05:27 +0100

    formulaire

--- src/App.vue
+++ src/App.vue
@@ -15,40 +15,26 @@
           <label for="inputEmail4">Titre de l'activite</label>
           <input type="email" class="form-control" id="inputEmail4" placeholder="Titre">
         </div>
-        <div class="form-inline">
-          <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Preference</label>
-          <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
-            <option selected>Choose...</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-          </select>
-
         <div class="form-group col-md-6">
-          <label for="inputPassword4">Password</label>
-          <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+          <label for="inlineFormCustomSelectPref">Preference</label>
+          <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
+            <option selected>choose an activity</option>
+            <option value="1">running</option>
+            <option value="2">road bike</option>
+            <option value="3">mountain bike</option>
+          </select>
         </div>
       </div>
 
-      <div class="form-group">
-        <label for="inputAddress">Address</label>
-        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-      </div>
-      <div class="form-group">
-        <label for="inputAddress2">Address 2</label>
-        <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-      </div>
       <div class="form-row">
         <div class="form-group col-md-6">
-          <label for="inputCity">City</label>
-          <input type="text" class="form-control" id="inputCity">
+          <label for="inputPassword4">Distance parcourue</label>
+          <input type="password" class="form-control" id="inputPassword4" placeholder="Distance ne km">
         </div>
-        <div class="form-group col-md-4">
-          <label for="inputState">State</label>
-          <select id="inputState" class="form-control">
-            <option selected>Choose...</option>
-            <option>...</option>
-          </select>
+
+        <div class="form-group col-md-6">
+          <label for="inputCity">City</label>
+          <input type="text" class="form-control" id="inputCity" placeholder=" ville">
         </div>
       </div>
       <div class="form-group">
